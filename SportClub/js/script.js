3768afb2e25177fe0bbee965314306d32966536c
Foks991 <33574193+Foks991@users.noreply.github.com>
2019-09-03 01:02:04 +0300

    Sport club add

---
+++ SportClub/js/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.hamburger').on('click', function(){
+  		$('.burger-menu').toggleClass("active");
+        $('.bar3, .bar2').css('background-color','black');
+  	});
+});
